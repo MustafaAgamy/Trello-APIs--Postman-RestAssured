8ed4b0742751a40b2a883aeeae4bcf527373bd5f
MustafaAgamy <czczc2009@gmail.com>
2023-03-25 15:27:49 +0200

    first commit

---
+++ TrelloAPIsWRestAssured/src/test/java/Trello/TrelloCreate.java
@@ -0,0 +1,92 @@
+package Trello;
+
+import io.restassured.http.ContentType;
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+
+public class TrelloCreate {
+
+    public static String key = "bb07493e2dcc111900fe6cd74894c5bf";
+    public static String token = "63935cf4f2d7f3ae3898502171fbade6808d639df94661bd65033fb6a3e1ca9d";
+
+
+    @Test(priority = 1)
+    public void createBoard(ITestContext contextBoard){
+
+        String idBoard =
+
+            given().
+                    contentType(ContentType.JSON)
+                    .with()
+                    .queryParam("name","First Board")
+                    .queryParam("key",key)
+                    .queryParam("token",token)
+                    .when()
+                    .post("https://api.trello.com/1/boards/")
+                    .jsonPath().getString("id");
+
+        System.out.println("Generated Board id is: " + idBoard);
+        contextBoard.setAttribute("boardId", idBoard);
+    }
+    @Test(priority = 2)
+    public void createList(ITestContext contextBoard,ITestContext contextList){
+
+        String idBoard = (String) contextBoard.getAttribute("boardId");
+        String idList =
+
+            given().
+                    contentType(ContentType.JSON)
+                    .with()
+                    .queryParam("name","First List")
+                    .queryParam("key",key)
+                    .queryParam("token",token)
+                    .queryParam("idBoard", idBoard)
+                    .when()
+                    .post("https://api.trello.com/1/lists/")
+                    .jsonPath().getString("id");
+
+        System.out.println("Generated List id is: " + idList);
+        contextList.setAttribute("listId", idList);
+    }
+    @Test(priority = 3)
+    public void createCard(ITestContext contextList,ITestContext contextCard){
+        String idList = (String) contextList.getAttribute("listId");
+        String idCard =
+
+            given().
+                    contentType(ContentType.JSON)
+                    .with()
+                    .queryParam("name","First Card")
+                    .queryParam("key",key)
+                    .queryParam("token",token)
+                    .queryParam("idList",idList)
+                    .when()
+                    .post("https://api.trello.com/1/cards/")
+                    .jsonPath().getString("id");
+
+        System.out.println("Generated Card id is: " + idCard);
+        contextCard.setAttribute("cardId", idCard);
+    }
+    @Test(priority = 4)
+    public void createChecklist(ITestContext contextCard,ITestContext contextChecklist){
+        String idCard = (String) contextCard.getAttribute("cardId");
+        String idChecklist =
+
+            given().
+                    contentType(ContentType.JSON)
+                    .with()
+                    .queryParam("name","First Board")
+                    .queryParam("key",key)
+                    .queryParam("token",token)
+                    .queryParam("idCard",idCard)
+                    .when()
+                    .post("https://api.trello.com/1/checklists/")
+                    .jsonPath().getString("id");
+
+        System.out.println("Generated Checklist id is: " + idChecklist);
+        contextChecklist.setAttribute("ChecklistId", idChecklist);
+    }
+}
